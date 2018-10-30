448f8cf6cfcd12c0bd55e452dd276bfe16f5432d
Matteo Baglini <matteo.baglini@gmail.com>
2018-10-30 16:28:39 +0100

    initial project plus first batch of exercises

---
+++ exercises/src/test/scala/00_CheckTests.scala
@@ -0,0 +1,12 @@
+package exercises
+
+import minitest._
+
+object CheckTests extends SimpleTestSuite {
+
+  test("it works!") {
+    val result   = "a value"
+    val expected = "a value"
+    assertEquals(result, expected)
+  }
+}
